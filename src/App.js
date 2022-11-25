1e1bcf8203d17c2f5bc8759136cf8ad65a056673
Tomás Rossi <91207352+rossitom90@users.noreply.github.com>
2022-11-25 16:54:52 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg'
 import './App.css'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Hero from './components/Hero'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Hero />
+    </BrowserRouter>
+  )
 }
 
 export default App
